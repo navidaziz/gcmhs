e47b4acb38d654a06e41af06038b5e68f96652bf
navidaziz <navidaziz98@gmail.com>
2021-06-19 17:07:16 +0500

    work on print

--- application/controllers/admin/timetable.php
+++ application/controllers/admin/timetable.php
@@ -72,7 +72,7 @@ class Timetable extends Admin_Controller
 		$query = "SELECT  `Class_title`, `class_id`
 					FROM
 					`classes` 
-					WHERE `class_id` IN(2,3,4,5,6)";
+					WHERE `class_id` IN(2,3,4,5)";
 
 		$result = $this->db->query($query);
 		$classes = $result->result();
