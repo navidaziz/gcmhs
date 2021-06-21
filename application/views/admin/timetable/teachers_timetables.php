5607a0387ef4ba646a31fb3bcc7189e571eca1e7
unknown <navidaziz98@gmail.com>
2021-06-21 11:29:00 +0500

    some changs

--- application/views/admin/timetable/teachers_timetables.php
+++ application/views/admin/timetable/teachers_timetables.php
@@ -39,7 +39,7 @@
           <h6>
 
             <?php foreach ($teachers as $teacher) { ?>
-              <div style="width: 46%; float: left; margin: 5px; margin-left: 5px; margin-bottom:10px !important">
+              <div style="width: 100%; float: left; margin: 5px; margin-left: 5px; margin-bottom:10px !important">
                 <table id="example" class="table table-bordered" cellspacing="0" width="100%" style="float:left; font-size:11px">
                   <thead>
                     <tr>
@@ -114,6 +114,8 @@
                   </tbody>
                 </table>
               </div>
+              <br />
+              <br />
 
             <?php } ?>
 
