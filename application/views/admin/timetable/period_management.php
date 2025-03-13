78065ad4b538759af5f1fbf67508bbba21246431
Navid Aziz <navidaziz98@gmail.com>
2025-03-13 23:28:14 +0500

    some changes

--- application/views/admin/timetable/period_management.php
+++ application/views/admin/timetable/period_management.php
@@ -548,10 +548,14 @@ AND `period_subjects`.`teacher_id`= '$teacher->teacher_id'";
 
         });
       </script>
-      <script type="text/javascript" src="http://gcmhsb.chitral.com.pk/assets/admin/datatable/jquery.dataTables.min.js"></script>
-      <!-- <link rel="stylesheet" type="text/css" href="http://gcmhsb.chitral.com.pk/assets/admin/datatable/jquery.data Tables.min.css" /> -->
-      <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
-      <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+      <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
+      <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
+      <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+      <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
+      <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
 
 </body>
 
