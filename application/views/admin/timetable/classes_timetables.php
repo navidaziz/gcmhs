1d3f089e17864d2a82d3b96f0f982eaa4e6bc02f
Navid Aziz <navidaziz98@gmail.com>
2025-03-24 11:17:07 +0500

    some changes

--- application/views/admin/timetable/classes_timetables.php
+++ application/views/admin/timetable/classes_timetables.php
@@ -42,17 +42,18 @@
                             <div class="col-md-6">
 
                               <h2>Time Table For Class: <strong><?php echo $class->Class_title; ?> <?php echo $section->section_title; ?> </strong></h2>
-                              <h4>Class Teacher:
-                                <?php $query = "SELECT teacher_name
+                              <h4><strong>Incharge Teacher:
+                                  <?php $query = "SELECT teacher_name
                                     FROM `classes_time_tables`
                                     WHERE `classes_time_tables`.`class_teacher`='1' 
                                     AND `classes_time_tables`.`class_id`='" . $class->class_id . "'
                                     AND  `classes_time_tables`.`section_id`='" . $section->section_id . "'";
-                                $class_teacher = $this->db->query($query)->result();
-                                if ($class_teacher) {
-                                  echo $class_teacher[0]->teacher_name;
-                                }
-                                ?>
+                                  $class_teacher = $this->db->query($query)->result();
+                                  if ($class_teacher) {
+                                    echo $class_teacher[0]->teacher_name;
+                                  }
+                                  ?>
+                                </strong>
                               </h4>
                               </strong>
 
