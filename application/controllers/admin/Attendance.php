01c9fd334a9520cedb355e0f758a83d559ef445e
Navid Aziz <navidaziz98@gmail.com>
2025-04-11 17:10:45 +0500

    some changes

--- application/controllers/admin/Attendance.php
+++ application/controllers/admin/Attendance.php
@@ -18,34 +18,7 @@ class Attendance extends CI_Controller
 	public function index()
 	{
 
-		/*var_dump($this->session->userdata('student_name'));
-		exit();
-		*/
-		$query = "SELECT * FROM `classes` WHERE status=1 ORDER BY class_id DESC";
 
-		$result = $this->db->query($query);
-		$classes = $result->result();
-		//var_dump($classes);
-
-		foreach ($classes as $classe) {
-			$query = "SELECT DISTINCT 
-						  `sections`.`section_id`,
-						  `sections`.`section_title`,
-						  `sections`.`color` 
-						FROM
-						  `students`,
-						  `sections` 
-						WHERE `students`.`section_id` = `sections`.`section_id`
-						AND `students`.`status` =1
-						AND `students`.`class_id` =" . $classe->class_id . " ORDER BY sections.section_id ASC";
-
-			$result = $this->db->query($query);
-			$sections = $result->result();
-			$classe->sections = $sections;
-		}
-
-		//var_dump($classes);
-		$this->data['classes'] = $classes;
 		$this->data['title'] = "Attendance";
 		$this->data["view"] = ADMIN_DIR . "attendance/home";
 		$this->load->view(ADMIN_DIR . "layout", $this->data);
--- application/views/admin/attendance/home.php
+++ application/views/admin/attendance/home.php
@@ -1,20 +1,78 @@
+<?php
+// Single optimized query to fetch all required data
+$query = "
+    SELECT 
+        c.class_id,
+        c.Class_title,
+        sec.section_id,
+        sec.section_title,
+        sec.color,
+        COUNT(CASE WHEN st.status = 1 THEN 1 END) as active_students,
+        COUNT(CASE WHEN st.status = 2 THEN 1 END) as struck_off_students,
+        COUNT(CASE WHEN st.status IN (1,2) THEN 1 END) as total_students
+    FROM 
+        classes c
+    LEFT JOIN 
+        students st ON st.class_id = c.class_id
+    LEFT JOIN 
+        sections sec ON sec.section_id = st.section_id AND st.status IN (1,2)
+    WHERE 
+        c.status = 1
+    GROUP BY 
+        c.class_id, sec.section_id
+    ORDER BY 
+        c.class_id DESC, sec.section_id ASC
+";
+
+$result = $this->db->query($query);
+$data = $result->result();
+
+// Organize data by class
+$classes = [];
+foreach ($data as $row) {
+  if (!isset($classes[$row->class_id])) {
+    $classes[$row->class_id] = (object)[
+      'class_id' => $row->class_id,
+      'Class_title' => $row->Class_title,
+      'total_students' => 0,
+      'sections' => []
+    ];
+  }
+
+  if ($row->section_id) {
+    $classes[$row->class_id]->sections[] = (object)[
+      'section_id' => $row->section_id,
+      'section_title' => $row->section_title,
+      'color' => $row->color,
+      'active_students' => $row->active_students,
+      'struck_off_students' => $row->struck_off_students
+    ];
+  }
+
+  $classes[$row->class_id]->total_students += $row->total_students;
+}
+
+// Get global counts
+$global_counts = $this->db->query("
+    SELECT 
+        COUNT(CASE WHEN status IN(1,2) AND class_id IN (2,3,4,5,6) THEN 1 END) as total_students_all,
+        COUNT(CASE WHEN status = 2 THEN 1 END) as total_struck_off_all
+    FROM students
+")->row();
+?>
 <!-- PAGE HEADER-->
 <div class="row">
   <div class="col-sm-12">
     <div class="page-header" style="min-height: 30px;">
-      <!-- STYLER -->
-
-      <!-- /STYLER -->
-      <!-- BREADCRUMBS -->
       <ul class="breadcrumb">
         <li>
           <i class="fa fa-home"></i>
-          <a href="<?php echo site_url(ADMIN_DIR . $this->session->userdata("role_homepage_uri")); ?>"><?php echo $this->lang->line('Home'); ?></a>
+          <a href="<?php echo site_url(ADMIN_DIR . $this->session->userdata("role_homepage_uri")); ?>">
+            <?php echo $this->lang->line('Home'); ?>
+          </a>
         </li>
         <li><?php echo $title; ?></li>
       </ul>
-
-
     </div>
   </div>
 </div>
@@ -22,123 +80,59 @@
 
 <!-- PAGE MAIN CONTENT -->
 <div class="row">
-  <!-- MESSENGER -->
   <div class="col-md-12">
     <div class="box border blue" id="messenger">
       <div class="box-title">
         <h4><i class="fa fa-book" aria-hidden="true"></i> <?php echo $title; ?></h4>
-        <!--<div class="tools">
-            
-				<a href="#box-config" data-toggle="modal" class="config">
-					<i class="fa fa-cog"></i>
-				</a>
-				<a href="javascript:;" class="reload">
-					<i class="fa fa-refresh"></i>
-				</a>
-				<a href="javascript:;" class="collapse">
-					<i class="fa fa-chevron-up"></i>
-				</a>
-				<a href="javascript:;" class="remove">
-					<i class="fa fa-times"></i>
-				</a>
-				
-
-			</div>-->
       </div>
       <div class="box-body">
-
         <div class="table-responsive">
           <div class="row">
             <div class="col-md-12">
-              <!-- MESSENGER -->
-
               <table class="table table-bordered">
                 <thead>
                   <tr>
-                    <th colspan="2">Classes-Sections -
-                      <small><?php $query = "SELECT COUNT(*) as total FROM students as s
-                              WHERE s.status IN(1,2) and s.class_id IN (2,3,4,5,6)";
-                              echo $this->db->query($query)->result()[0]->total;
-                              ?>
-                      </small>
-
-                    </th>
+                    <th colspan="2">Classes-Sections - <small><?php echo $global_counts->total_students_all; ?></small></th>
                     <th>Total</th>
-                    <th title="struck Off"> Struck Off - <?php $query = "SELECT COUNT(*) as total FROM students as s
-                              WHERE s.status IN(2)";
-                                                          echo $this->db->query($query)->result()[0]->total;
-                                                          ?>
-                    </th>
-
+                    <th title="struck Off">Struck Off - <?php echo $global_counts->total_struck_off_all; ?></th>
                   </tr>
                 </thead>
-                </body>
-                <?php foreach ($classes as $class) { ?>
+                <tbody>
+                  <?php foreach ($classes as $class): ?>
+                    <?php if (!empty($class->sections)): ?>
+                      <?php foreach ($class->sections as $index => $section): ?>
+                        <tr>
+                          <?php if ($index === 0): ?>
+                            <th style="text-align: center;" rowspan="<?php echo count($class->sections); ?>">
+                              <?php echo $class->Class_title; ?>
+                              <br /><small><?php echo $class->total_students; ?></small>
+                            </th>
+                          <?php endif; ?>
 
-                  <?php
-                  $count = 1;
-                  foreach ($class->sections as $section) { ?>
-                    <tr>
-                      <?php if ($count == 1) { ?>
-                        <th style="text-align: center;" rowspan="<?php echo count($class->sections); ?>"><?php echo $class->Class_title; ?>
-                          <br />
-                          <small><?php $query = "SELECT COUNT(*) as total FROM students as s
-                              WHERE s.class_id = $class->class_id
-                              AND s.status IN(1,2)";
-                                  echo $this->db->query($query)->result()[0]->total;
-                                  ?>
-                          </small>
+                          <td style="background-color: <?php echo $section->color; ?>;">
+                            <?php echo $section->section_title; ?>
+                          </td>
 
-                        </th>
-                      <?php
-                        $count++;
-                      } ?>
+                          <th style="text-align: center;">
+                            <?php echo $section->active_students; ?>
+                          </th>
 
-
-                      <td style="background-color: <?php echo $section->color; ?>;"><?php echo $section->section_title; ?></td>
-
-                      <th style="text-align: center;">
-                        <?php $query = "SELECT COUNT(*) as total FROM students as s
-                              WHERE s.class_id = $class->class_id
-                              AND s.section_id = $section->section_id
-                              AND s.status = 1";
-                        echo $this->db->query($query)->result()[0]->total;
-                        ?>
-                      </th>
-
-                      <td style="text-align: center;">
-                        <a href="<?php echo site_url(ADMIN_DIR . "admission/struck_off_students") . "/$class->class_id/$section->section_id"; ?>">
-                          <?php $query = "SELECT COUNT(*) as total FROM students as s
-                              WHERE s.class_id = $class->class_id
-                              AND s.section_id = $section->section_id
-                              AND s.status = 2";
-                          echo $this->db->query($query)->result()[0]->total;
-                          ?> <i class="fa fa-eye"></i></a>
-
-
-                      </td>
-
-
-                    </tr>
-                  <?php } ?>
-
-
-                <?php } ?>
+                          <td style="text-align: center;">
+                            <a href="<?php echo site_url(ADMIN_DIR . "admission/struck_off_students/{$class->class_id}/{$section->section_id}"); ?>">
+                              <?php echo $section->struck_off_students; ?>
+                              <i class="fa fa-eye"></i>
+                            </a>
+                          </td>
+                        </tr>
+                      <?php endforeach; ?>
+                    <?php endif; ?>
+                  <?php endforeach; ?>
+                </tbody>
               </table>
-
             </div>
-
-
           </div>
-
-
-
         </div>
-
-
       </div>
-
     </div>
   </div>
-  <!-- /MESSENGER -->
 </div>
