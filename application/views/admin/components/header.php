a46d6e04ff95f7acd1c533cc63667fa5a466d5be
Navid Aziz <navidaziz98@gmail.com>
2025-04-11 20:57:58 +0500

    some changes

--- application/views/admin/components/header.php
+++ application/views/admin/components/header.php
@@ -162,7 +162,7 @@
           <img src="<?php echo site_url("assets/uploads/" . $system_global_settings[0]->sytem_admin_logo); ?>" alt="<?php echo $system_global_settings[0]->system_title ?>" title="<?php echo $system_global_settings[0]->system_title ?>" class="img-responsive " style="width:40px; height:40px !important;"></a>
         <!-- /COMPANY LOGO -->
         <!-- TEAM STATUS FOR MOBILE -->
-        <div style="display: none;" class="visible-xs "> <a href="#" class="team-status-toggle switcher btn dropdown-toggle"> <i class="fa fa-users"></i> </a> </div>
+        <div class="visible-xs "> <a href="#" class="team-status-toggle switcher btn dropdown-toggle"> <i class="fa fa-users"></i> </a> </div>
         <!-- /TEAM STATUS FOR MOBILE -->
         <!-- SIDEBAR COLLAPSE -->
         <div id="sidebar-collapse" class="sidebar-collapse btn"> <i class="fa fa-bars" data-icon1="fa fa-bars" data-icon2="fa fa-bars"></i> </div>
