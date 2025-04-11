32701b6965c1a8034002c029a2c99f78e03d4066
Navid Aziz <navidaziz98@gmail.com>
2025-04-11 20:56:17 +0500

    some changes

--- application/views/admin/components/header.php
+++ application/views/admin/components/header.php
@@ -165,7 +165,7 @@
         <div style="display: none;" class="visible-xs "> <a href="#" class="team-status-toggle switcher btn dropdown-toggle"> <i class="fa fa-users"></i> </a> </div>
         <!-- /TEAM STATUS FOR MOBILE -->
         <!-- SIDEBAR COLLAPSE -->
-        <div id="sidebar-collapse" class="sidebar-collapse btn"> <i class="fa fa-bars" data-icon1="fa fa-bars" data-icon2="fa fa-bars"></i> </div>
+        <div style="display: none !important;" id="sidebar-collapse" class="sidebar-collapse btn"> <i class="fa fa-bars" data-icon1="fa fa-bars" data-icon2="fa fa-bars"></i> </div>
         <!-- /SIDEBAR COLLAPSE -->
       </div>
       <!-- <div class="nav navbar-nav pull-left">
