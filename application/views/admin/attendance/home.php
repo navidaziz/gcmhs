af4a1ef816419e758273b2e7929c881c80571c91
Navid Aziz <navidaziz98@gmail.com>
2025-04-11 19:33:42 +0500

    some changes

--- application/views/admin/attendance/home.php
+++ application/views/admin/attendance/home.php
@@ -92,15 +92,13 @@ $global_counts = $this->db->query("
         <div class="table-responsive">
 
           <?php
-          $query = "SELECT sum(present) as present, 
-                                   sum(`leave`) as `leave`, 
-                                   sum(absent) as absent, 
-                                   sum(total) as total, 
-                                   sum(total_students) as total_students, 
-                                   sum(strucked_off) as strucked_off, 
-                                   (sum(total)*100) / sum(total_students) as total_attendance_percentage 
-                            FROM `today_attendance_summery`";
-          $today_attendance_summary = $this->db->query($query)->result()[0];
+          $query = "SELECT sum(`present`) as `present`,
+                        sum(`leave`) as `leave`,
+                        sum(`absent`) as `absent`,
+                        sum(`evening_absent`) as evening_absent
+                         FROM `daily_class_wise_attendance` 
+                                WHERE  DATE(`date`) = DATE(Now());";;
+          $today_attendance_summary = $this->db->query($query)->row();
 
 
           ?>
