b0c3be5731c783c96840047f85e4caf7ec92bc3c
Navid Aziz <navidaziz98@gmail.com>
2025-05-12 11:05:41 +0500

    some changes

--- application/views/admin/school_dashboard/dashboard.php
+++ application/views/admin/school_dashboard/dashboard.php
@@ -786,5 +786,14 @@ foreach ($todaySummary as $t) {
         targets: 0 // Disable sorting for the first column
       }]
     });
+    table.on('order.dt search.dt', function() {
+      table.column(0, {
+        search: 'applied',
+        order: 'applied'
+      }).nodes().each(function(cell, i) {
+        cell.innerHTML = i + 1;
+        table.cell(cell).invalidate('dom');
+      });
+    }).draw();
   });
 </script>
