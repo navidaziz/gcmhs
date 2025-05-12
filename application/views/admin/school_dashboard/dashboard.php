e5ce869caeb1b99be17ab707865dcf35fc28f924
Navid Aziz <navidaziz98@gmail.com>
2025-05-12 11:08:14 +0500

    some changes

--- application/views/admin/school_dashboard/dashboard.php
+++ application/views/admin/school_dashboard/dashboard.php
@@ -783,17 +783,21 @@ foreach ($todaySummary as $t) {
       columnDefs: [{
         searchable: false,
         orderable: false,
-        targets: 0 // Disable sorting for the first column
-      }]
+        targets: 0 // First column (Serial No.) not sortable/searchable
+      }],
+      order: [] // Optional: avoid initial sorting
     });
-    table.on('order.dt search.dt', function() {
+
+    // Auto-indexing the first column (Serial No.)
+    table.on('order.dt search.dt draw.dt', function() {
       table.column(0, {
-        search: 'applied',
-        order: 'applied'
-      }).nodes().each(function(cell, i) {
-        cell.innerHTML = i + 1;
-        table.cell(cell).invalidate('dom');
-      });
+          search: 'applied',
+          order: 'applied'
+        })
+        .nodes()
+        .each(function(cell, i) {
+          cell.innerHTML = i + 1;
+        });
     }).draw();
   });
 </script>
