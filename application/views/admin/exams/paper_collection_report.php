79badd37edb6b125783b491a2f92dfa2587087c9
Navid Aziz <navidaziz98@gmail.com>
2025-05-29 20:17:10 +0500

    some changes

--- application/views/admin/exams/paper_collection_report.php
+++ application/views/admin/exams/paper_collection_report.php
@@ -82,7 +82,7 @@
                           $assigned_teacher = $result->row();
                           if ($assigned_teacher->teacher_name) {
 
-                            $query = "SELECT COUNT(*) as total
+                            echo $query = "SELECT COUNT(*) as total
                                     FROM `students_exams_subjects_marks` 
                                     WHERE class_id = ? AND class_id = ? AND exam_id = ? AND subject_id = ?";
                             $result = $this->db->query($query, [$class->class_id, $section->section_id, $exam_id, $subject->subject_id]);
