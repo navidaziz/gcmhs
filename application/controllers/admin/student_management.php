add94f3827d952b4cb286ff898533560678f1b7e
Navid Aziz <navidaziz98@gmail.com>
2025-09-23 19:18:44 +0500

    some chagnes

--- application/controllers/admin/student_management.php
+++ application/controllers/admin/student_management.php
@@ -21,4 +21,34 @@ class Student_management extends Admin_Controller
 		$this->data["view"] = ADMIN_DIR . "student_management/home";
 		$this->load->view(ADMIN_DIR . "layout", $this->data);
 	}
+
+
+	public function view_student_list($class_id, $section_id)
+	{
+		// Cast IDs to integers for security
+		$class_id   = (int) $class_id;
+		$section_id = (int) $section_id;
+
+		$this->data['class_id']   = $class_id;
+		$this->data['section_id'] = $section_id;
+
+		$query = $this->db->query("
+        SELECT s.*, c.Class_title, sec.section_title 
+        FROM students s
+        JOIN classes c ON s.class_id = c.class_id
+        JOIN sections sec ON s.section_id = sec.section_id
+        WHERE s.class_id = ? AND s.section_id = ?
+    	", array($class_id, $section_id));
+
+		$class_section = $query->row();
+
+		if ($class_section) {
+			$this->data['title'] = $class_section->Class_title . " - " . $class_section->section_title . " Students List";
+		} else {
+			$this->data['title'] = "Students List";
+		}
+
+		$this->data["view"] = ADMIN_DIR . "student_management/class_section_students_list";
+		$this->load->view(ADMIN_DIR . "layout", $this->data);
+	}
 }
---
+++ application/views/admin/student_management/class_section_students_list.php
@@ -0,0 +1,145 @@
+<style>
+    .table_small>thead>tr>th,
+    .table_small>tbody>tr>th,
+    .table_small>tfoot>tr>th,
+    .table_small>thead>tr>td,
+    .table_small>tbody>tr>td,
+    .table_small>tfoot>tr>td {
+        padding: 4px;
+        line-height: 1;
+        vertical-align: top;
+        border-top: 1px solid #ddd;
+        font-size: 12px !important;
+        color: black;
+        margin: 0px !important;
+    }
+</style>
+
+<?php
+// Single optimized query to fetch all required data
+$query = "
+    SELECT 
+        c.class_id,
+        c.Class_title,
+        sec.section_id,
+        sec.section_title,
+        sec.color,
+        COUNT(CASE WHEN st.status = 1 THEN 1 END) as active_students,
+        COUNT(CASE WHEN st.status = 2 THEN 1 END) as struck_off_students,
+        COUNT(CASE WHEN st.status IN (1,2) THEN 1 END) as total_students
+    FROM 
+        classes c
+    LEFT JOIN 
+        students st ON st.class_id = c.class_id
+    LEFT JOIN 
+        sections sec ON sec.section_id = st.section_id AND st.status IN (1,2)
+    WHERE 
+        c.status = 1
+    GROUP BY 
+        c.class_id, sec.section_id
+    ORDER BY 
+        c.class_id DESC, sec.section_id ASC
+";
+
+$result = $this->db->query($query);
+$data = $result->result();
+
+// Organize data by class
+$classes = [];
+foreach ($data as $row) {
+    if (!isset($classes[$row->class_id])) {
+        $classes[$row->class_id] = (object)[
+            'class_id' => $row->class_id,
+            'Class_title' => $row->Class_title,
+            'total_students' => 0,
+            'struck_off_students' => 0,
+            'sections' => []
+        ];
+    }
+
+    if ($row->section_id) {
+        $classes[$row->class_id]->sections[] = (object)[
+            'section_id' => $row->section_id,
+            'section_title' => $row->section_title,
+            'color' => $row->color,
+            'active_students' => $row->active_students,
+            'struck_off_students' => $row->struck_off_students
+        ];
+    }
+
+    $classes[$row->class_id]->total_students += $row->total_students;
+    $classes[$row->class_id]->struck_off_students += $row->struck_off_students;
+}
+
+?>
+
+<!-- PAGE HEADER-->
+<div class="row">
+    <div class="col-sm-12">
+        <div class="page-header" style="min-height: 30px;">
+            <ul class="breadcrumb">
+                <li>
+                    <i class="fa fa-home"></i>
+                    <a href="<?php echo site_url(ADMIN_DIR . $this->session->userdata("role_homepage_uri")); ?>">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <i class="fa fa-home"></i>
+                    <a href="<?php echo site_url(ADMIN_DIR . "student_management"); ?>">
+                        Dashboard
+                    </a>
+                </li>
+                <li><?php echo $title; ?></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- /PAGE HEADER -->
+
+<!-- PAGE MAIN CONTENT -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="box border blue" id="messenger">
+
+            <div class="box-body">
+                <div class="table-responsive">
+                    <table id="example" class="table table-bordered table-striped ">
+
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Student ID</th>
+                                <th>Class No.</th>
+                                <th>Addmission No</th>
+                                <th>Student Info</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <?php
+                            $count = 1;
+                            $query = $this->db->query("SELECT * FROM students WHERE status IN (1,2)
+                            AND class_id = ? and section_id = ?", array($class_id, $section_id));
+                            $students = $query->result();
+
+                            foreach ($students as $student): ?>
+                                <tr>
+                                    <td><?php echo $count++; ?></td>
+                                    <td><?php echo $student->student_id; ?></td>
+                                    <td><?php echo $student->class_id; ?></td>
+                                    <td><?php echo $student->admission_no; ?></td>
+                                    <td><?php echo $student->first_name . " " . $student->last_name; ?></td>
+                                    <td>
+                                        <button class="btn btn-success">Review Info</button>
+                                    </td>
+                                </tr>
+
+                            <?php endforeach; ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
