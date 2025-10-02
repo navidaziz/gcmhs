bc1d238d5d486f350c74a84a14829278f031700f
Navid Aziz <navidaziz98@gmail.com>
2025-10-02 18:30:09 +0500

    some changes

--- application/views/admin/admission/dmc/print_dmc.php
+++ application/views/admin/admission/dmc/print_dmc.php
@@ -325,7 +325,7 @@
 
         <!-- ATTENDANCE -->
         <h4>Attendance History</h4>
-        <table class="table table-bordered">
+        <table class="table table-bordered table_small">
             <thead>
                 <tr>
                     <th>Month / Days</th>
