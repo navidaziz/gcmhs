c215966be211af06c3c36e2face52a2c47ed4072
Navid Aziz <navidaziz98@gmail.com>
2025-12-04 20:57:07 +0500

    some chagnes

--- application/controllers/admin/school_dashboard.php
+++ application/controllers/admin/school_dashboard.php
@@ -44,7 +44,7 @@ class School_dashboard extends Admin_Controller
         FROM classes AS c
         INNER JOIN class_sections AS cs ON cs.class_id = c.class_id
         INNER JOIN sections AS s ON s.section_id = cs.section_id
-        WHERE s.status = 1 AND c.status = 1 AND c.class_id IN (2,3,4,5)
+        WHERE s.status = 1 AND c.status = 1 AND c.class_id IN (2,3,4,5,6)
         GROUP BY c.class_id, s.section_id
     ";
 
