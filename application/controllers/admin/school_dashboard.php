513c01a668b9976551f386cf509fa5f83a721ecb
Navid Aziz <navidaziz98@gmail.com>
2025-12-04 20:27:55 +0500

    some changes

--- application/controllers/admin/school_dashboard.php
+++ application/controllers/admin/school_dashboard.php
@@ -34,43 +34,49 @@ class School_dashboard extends Admin_Controller
 
 	public function missing_attendance()
 	{
-		$class_id = 5;
-		$section_id = 1;
 
-		echo "<table border='1' cellpadding='5' cellspacing='0'>";
+		$query = "SELECT c.class_id, c.Class_title, s.section_id, s.section_title FROM `classes` as c INNER JOIN class_sections as cs ON cs.class_id = c.class_id INNER JOIN `sections` as s ON s.section_id = cs.section_id WHERE s.status=1 AND c.status=1 AND c.class_id IN(2,3,4,5,6) GROUP BY c.class_id , s.section_id;";
+		$classes = $this->db->query($query)->result();
+		foreach ($classes as $class) {
 
-		// Header row
-		echo "<tr><th>Month</th>";
-		for ($day = 1; $day <= 31; $day++) {
-			echo "<th>$day</th>";
-		}
-		echo "</tr>";
+			$class_id = $class->class_id;
+			$section_id = $class->section_id;
+			echo "<h4>" . $class->Class_title . " - " . $class->section_title . "</h4>";
+			echo "<hr />";
+			echo "<table border='1' cellpadding='5' cellspacing='0'>";
 
-		$year = date('Y');
-
-		for ($month = 4; $month <= 12; $month++) {
-
-			echo "<tr>";
-			echo "<td>" . date('F', mktime(0, 0, 0, $month, 1)) . "</td>";
-
+			// Header row
+			echo "<tr><th>Month</th>";
 			for ($day = 1; $day <= 31; $day++) {
+				echo "<th>$day</th>";
+			}
+			echo "</tr>";
 
-				// Skip invalid days (e.g., Feb 30, April 31)
-				if (!checkdate($month, $day, $year)) {
-					echo "<td style='background-color:#eee;'>X</td>";
-					continue;
-				}
+			$year = date('Y');
 
-				$timestamp = strtotime("$year-$month-$day");
+			for ($month = 4; $month <= 12; $month++) {
 
-				// Check if Sunday
-				if (date('w', $timestamp) == 0) {
-					echo "<td style='background-color:#d9d9d9; color:red;'>Sun</td>";
-					continue;
-				}
+				echo "<tr>";
+				echo "<td>" . date('F', mktime(0, 0, 0, $month, 1)) . "</td>";
 
-				// Query attendance
-				$query = $this->db->query("
+				for ($day = 1; $day <= 31; $day++) {
+
+					// Skip invalid days (e.g., Feb 30, April 31)
+					if (!checkdate($month, $day, $year)) {
+						echo "<td style='background-color:#eee;'>X</td>";
+						continue;
+					}
+
+					$timestamp = strtotime("$year-$month-$day");
+
+					// Check if Sunday
+					if (date('w', $timestamp) == 0) {
+						echo "<td style='background-color:#d9d9d9; color:red;'>Sun</td>";
+						continue;
+					}
+
+					// Query attendance
+					$query = $this->db->query("
                 SELECT * FROM students_attendance
                 WHERE DAY(`date`) = $day
                 AND MONTH(`date`) = $month
@@ -79,18 +85,19 @@ class School_dashboard extends Admin_Controller
                 AND section_id = $section_id
             ");
 
-				$count = $query->num_rows();
+					$count = $query->num_rows();
 
-				if ($count > 0) {
-					echo "<td style='background-color:red; color:white;'>$count</td>";
-				} else {
-					echo "<td>-</td>";
+					if ($count > 0) {
+						echo "<td style='background-color:red; color:white;'>$count</td>";
+					} else {
+						echo "<td>-</td>";
+					}
 				}
+
+				echo "</tr>";
 			}
 
-			echo "</tr>";
+			echo "</table>";
 		}
-
-		echo "</table>";
 	}
 }
