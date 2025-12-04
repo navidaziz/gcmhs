accbb2019a06b14a77469eb3272117ee0dd873e6
Navid Aziz <navidaziz98@gmail.com>
2025-12-04 20:10:00 +0500

    some changes

--- application/controllers/admin/school_dashboard.php
+++ application/controllers/admin/school_dashboard.php
@@ -34,7 +34,30 @@ class School_dashboard extends Admin_Controller
 
 	public function missing_attendance()
 	{
-		$this->load->library('calendar');
-		echo $this->calendar->generate();
+		// $this->load->library('calendar');
+		// echo $this->calendar->generate();
+		echo "<table border='1' cellpadding='5' cellspacing='0'>";
+		echo "<tr><th>Month</th>";
+		for ($day = 1; $day <= 31; $day++) {
+
+			echo "<td>" . $day . "</td>";
+		}
+		echo "</tr>";
+
+		for ($month = 1; $month <= 1; $month++) {
+			echo "<tr><td>" . date('F', mktime(0, 0, 0, $month, 10)) . "</td>";
+			for ($day = 1; $day <= 31; $day++) {
+				$query = $this->db->query("SELECT * FROM `student_attendances` 
+				WHERE DAY(`attendance_date`) = $day AND MONTH(`attendance_date`) = $month 
+				AND YEAR(`attendance_date`) = " . date('Y'));
+				$attendance = $query->result();
+				if (!empty($attendance)) {
+					echo "<td style='background-color: red; color: white;'>" . count($attendance) . "</td>";
+				} else {
+					echo "<td> - </td>";
+				}
+				echo "</td>";
+			}
+		}
 	}
 }
