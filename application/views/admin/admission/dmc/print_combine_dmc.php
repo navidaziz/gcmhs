4cfdeb09b42ccaffdf24d4d152fbb0b854c236d7
Navid Aziz <navidaziz98@gmail.com>
2025-12-18 10:08:10 +0500

    some changes

--- application/views/admin/admission/dmc/print_combine_dmc.php
+++ application/views/admin/admission/dmc/print_combine_dmc.php
@@ -183,6 +183,15 @@
             background: #e9e9e9;
             font-weight: bold;
         }
+
+        .checkbox {
+            display: inline-block;
+            width: 16px;
+            height: 16px;
+            border: 1px solid #000;
+            vertical-align: middle;
+            margin: 0 5px;
+        }
     </style>
 </head>
 
