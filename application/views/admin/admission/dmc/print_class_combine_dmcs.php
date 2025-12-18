3fff2a3480107d503733ba954f2869c82eae9e23
Navid Aziz <navidaziz98@gmail.com>
2025-12-18 20:37:53 +0500

    some changes

--- application/views/admin/admission/dmc/print_class_combine_dmcs.php
+++ application/views/admin/admission/dmc/print_class_combine_dmcs.php
@@ -232,7 +232,7 @@
                                 <h5><strong>Progress Report</strong></h5>
 
                                 <?php
-                                echo $exam_id;
+                                echo $student_id;
                                 // === QUERY: Exam Info ===
                                 $query = "SELECT 
                             ex.year,
